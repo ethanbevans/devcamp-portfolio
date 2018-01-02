76129c67f8f464b512fc3be752d04a4b4bcbfaff
Ethan Evans <ethanbevans@gmail.com>
2018-01-02 05:13:29 -0500

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
 			content_tag(:p, greeting, class: "source-greeting")
 		end
 	end
+
+	def copyright_generator
+		EvansViewTool::Renderer.copyright 'Ethan Evans', 'All Rights Reserved'
+	end
 end
